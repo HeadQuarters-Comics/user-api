65cf9a0047812e2880a00bb3dfb5207c06fbc1f2
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-11-09 12:36:48 -0300

    Connect to db is working

--- db.js
+++ db.js
@@ -9,8 +9,27 @@ const db  = new Client({
 
 // --------- Simple Connection
 
-db.connect()
-db.query("select * from users").then(results => {
-    const result = results.rows
-    console.table(result)
-}).finally(() => db.end())
+//db.connect()
+//db.query("select * from users").then(results => {
+//    const result = results.rows
+//    console.table(result)
+//}).finally(() => db.end())
+
+async function getUsers() {
+    try{
+        console.log("Starting connection...")
+        await db.connect()
+        console.log("Success to connect!")
+        const result = await (await db.query("select * from users")).rows
+        console.table(result)
+    }
+    catch{
+        console.log("Error to getUsers! Erro" + ex)
+    }
+    finally{
+        await db.end()
+        console.log("Finish connection to db")
+    }
+}
+
+getUsers()
